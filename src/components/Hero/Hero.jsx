3aaa6013546d22b475acdaf5443f5433224b90e6
Amir H <amirhanan161@gmail.com>
2024-12-20 13:16:31 +0400

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Artyom</h1>
+        <p className={styles.description}>
+        I am a backend developer with experience in PHP, Laravel, and SQL databases. I enjoy building reliable and efficient solutions, with a strong focus on code quality and process optimization.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/ejik.jpeg")}
+        alt="Image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
